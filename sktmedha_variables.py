3d9cb9cf25d816bb04ff0bf5d56e3086297389b7
Satya Kumar T <sktmedha@gmail.com>
2026-05-02 13:35:22 +0530

    Add variable naming guidelines and initial variables

---
+++ sktmedha_variables.py
@@ -0,0 +1,9 @@
+# 4 Pillars of Variable Naming:
+# 1. No Reserved Words
+# 2. 63-card deck (A-Z, a-z, 0-9, _)
+# 3. Case matters (Love != love)
+# 4. Start with letter/underscore (No Digits)
+
+age = 0
+is_active = True
+greeting = "Hello, world!"
